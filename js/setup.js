80429abd172774176143011454f6a34589b9ad69
Tatyana Zima <tatjana.gruzd@yandex.ru>
2017-04-02 05:44:40 +0300

    Создан случайным образом набор волшебников

---
+++ js/setup.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+
+var randElement = function (mass) {
+  return mass[Math.floor(Math.random() * mass.length)];
+};
+
+var wizards = [];
+
+function createWizard() {
+  var wizard = {};
+
+  wizard.name = randElement(WIZARD_NAMES) + ' ' + randElement(WIZARD_SURNAMES);
+  wizard.coatColor = randElement(WIZARD_COAT_COLOR);
+  wizard.eyesColor = randElement(WIZARD_EYES_COLOR);
+
+  return wizard;
+}
+
+for (var i = 0; i < 4; i++) {
+  wizards.splice(i, 1, createWizard());
+}
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
